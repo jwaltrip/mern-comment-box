817f31c5fdbd3ad7e00ec027b57729e3129858ec
jwaltrip <jake.waltrip@gmail.com>
2018-08-19 11:27:59 -0500

    changed CommentForm from function to class extension

--- client/src/CommentForm.js
+++ client/src/CommentForm.js
@@ -1,26 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 // component that contains the author and comment text fields, and submit button
-const CommentForm = props => (
-  <form onSubmit={props.submitComment}>
-    <input
-      type="text"
-      name="author"
-      placeholder="Your name…"
-      value={props.author}
-      onChange={props.handleChangeText}
-    />
-    <input
-      type="text"
-      name="text"
-      placeholder="Say something..."
-      value={props.text}
-      onChange={props.handleChangeText /* this originally was: props.handleTextChange (i believe this was a typo) */}
-    />
-    <button type="submit">Submit</button>
-  </form>
-);
+
+// const CommentForm = (props) => (
+//   <form onSubmit={props.submitComment}>
+//     <input
+//       type="text"
+//       name="author"
+//       placeholder="Your name…"
+//       value={props.author}
+//       onChange={props.handleChangeText}
+//     />
+//     <input
+//       type="text"
+//       name="text"
+//       placeholder="Say something..."
+//       value={props.text}
+//       onChange={props.handleChangeText /* this originally was: props.handleTextChange (i believe this was a typo) */}
+//     />
+//     <button type="submit">Submit</button>
+//   </form>
+// );
+
+class CommentForm extends Component {
+  // constructor() {
+  //   super();
+  // }
+  render() {
+    return (
+      <form onSubmit={this.props.submitComment}>
+        <input
+          type="text"
+          name="author"
+          placeholder="Your name…"
+          value={this.props.author}
+          onChange={this.props.handleChangeText}
+        />
+        <input
+          type="text"
+          name="text"
+          placeholder="Say something..."
+          value={this.props.text}
+          onChange={this.props.handleChangeText /* this originally was: props.handleTextChange (i believe this was a typo) */}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
+}
 
 CommentForm.propTypes = {
   submitComment: PropTypes.func.isRequired,
